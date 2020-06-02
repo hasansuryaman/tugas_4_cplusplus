349d4f1f389f0a9290388309b8f10e985e46c8ea
hasansuryaman <hasan.suryaman@outlook.com>
2020-06-02 08:56:40 +0700

    Tugas 4 C++

---
+++ tugas04.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int x = 30;
+    cout <<"Nilai Awal X : " <<x <<endl;
+    x--;
+    cout <<"Nilai Kedua X :" <<x <<endl;
+    x++, x++;
+    cout <<"Nilai Ketiga X : " <<x <<endl;
+}
